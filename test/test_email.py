1b71610133f652a583370451df6e00290349dbb3
Brea Harper <harpertheauthor@Breas-MBP.home>
2026-04-23 00:27:34 -0400

    Use deployed email logo asset

--- test/test_email.py
+++ test/test_email.py
@@ -15,7 +15,7 @@ from app.services import email as email_service
             [
                 "Verify your ManifestBank email",
                 "Email Verification",
-                "manifestbank-app-logo-tight.png",
+                "manifestbank-app-logo-latest.png",
                 "Verify email",
                 "/verify-email?token=verify-token",
             ],
